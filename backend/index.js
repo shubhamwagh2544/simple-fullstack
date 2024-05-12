b4e84ccfca559fef724a22239a195e9c65c1ed48
shubham.wagh <shubham.wagh@globant.com>
2024-05-12 15:49:48 +0530

    get user by id

--- backend/index.js
+++ backend/index.js
@@ -39,4 +39,32 @@ app.get('/users', async (req, res) => {
     }
 })
 
+app.get('/user/:id', async (req, res) => {
+    try {
+        const userId = req.params.id
+        if (!userId) {
+            return res.status(409).json({
+                message: "invalid userId"
+            })
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not Found'
+            })
+        }
+        return res.status(200).json(user)
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
 app.listen(3000, () => console.log('server started on port 3000'))
