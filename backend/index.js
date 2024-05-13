29f727aa3c113b3486653eadc549beda8e1fe586
shubham.wagh <shubham.wagh@globant.com>
2024-05-13 13:04:57 +0530

    backend dockerfile

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+    // pass control to next handlers
+    next()
 })
 
 // health check endpoint
@@ -39,7 +42,7 @@ app.get('/users', async (req, res) => {
     }
 })
 
-app.get('/user/:id', async (req, res) => {
+app.get('/users/:id', async (req, res) => {
     try {
         const userId = req.params.id
         if (!userId) {
@@ -65,6 +68,80 @@ app.get('/user/:id', async (req, res) => {
             message: err.message
         })
     }
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const { name, email } = req.body
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email
+            }
+        })
+        return res.status(201).json(newUser)
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const userId = req.params.id
+        if (!userId) {
+            return res.status(409).json({
+                message: "invalid userId"
+            })
+        }
+        const { name } = req.body
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                name: name
+            }
+        })
+        return res.status(200).json(updatedUser)
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const userId = req.params.id
+        if (!userId) {
+            return res.status(409).json({
+                message: "invalid userId"
+            })
+        }
+        await prisma.user.delete({
+            where: {
+                id: userId
+            }
+        })
+        return res.status(200).json({
+            message: 'user deleted'
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
+app.all("*", (req, res) => {
+    res.status(200).json({
+        message: 'this might not page you are looking for:'
+    })
 })
 
 app.listen(3000, () => console.log('server started on port 3000'))
